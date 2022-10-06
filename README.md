0fb595355696bd435ada4b1105a054f362925916
Alex Boyd <40293068+alex-d-boyd@users.noreply.github.com>
2022-10-06 17:14:52 +0900

    Set up repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Advent of Code 2022
  My solutions for Advent of Code 2022
+
+Giving this a shot as a bit of practice and a bit of fun, after I was invited by a cow-orker last year.
---
+++ base.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+# Advent of Code
+# https://adventofcode.com/2022
+# Day 
+
+import argparse
+
+from pathlib import Path
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='AoC 2022 Day XX')
+    parser.add_argument('-t', '--test', help='use test data', action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.test:
+        in_file = Path('test.txt')
+    else:
+        in_file = Path('input.txt')
+    puzzle_input = in_file.read_text(encoding='utf-8')
+
